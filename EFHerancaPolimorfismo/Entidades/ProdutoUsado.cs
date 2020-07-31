a88901e11fd9e8a5b5a3ac8b54baabc548b8aee5
Angelo Carvalho <angelosuporteti@gmail.com>
2020-07-31 16:02:10 -0300

    Corrigido erro no formato da data no ToString da classe ProdudoUsado

--- EFHerancaPolimorfismo/Entidades/ProdutoUsado.cs
+++ EFHerancaPolimorfismo/Entidades/ProdutoUsado.cs
@@ -19,7 +19,7 @@ namespace EFHerancaPolimorfismo.Entidades
         public override string Etiqueta()
         {
             return Nome + "(usado) $ " + Preco.ToString("F2", CultureInfo.InvariantCulture)
-                + " (Data de fabricação: " + DataDeFabricacao.ToString("dd/MM/aaaa") + ")";
+                + " (Data de fabricação: " + DataDeFabricacao.ToString("dd/MM/yyyy") + ")";
         }
     }
 }
